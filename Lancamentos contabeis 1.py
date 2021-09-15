a7fea103f8cc937f924b3974a00dbae45dfc1780
jardelcruz1 <jardelcruz@gmail.com>
2021-09-14 21:52:08 -0300

    versao 2 lancamentos contabeis

--- Lancamentos contabeis 1.py
+++ Lancamentos contabeis 1.py
@@ -2,12 +2,12 @@
 
 Debito = int(input('Digite a conta debito:')) # aqui será inserido conta cotábil débito (ATIVO OU DESPESA)
 Credito = int(input('Digite a conta credito:')) # aqui será inserido conta cotábil crédito (passivo OU receita)
-Valor = float(input('Digite o valor:'))# o valor
+Valor = float(input('Digite o valor:'))# o valor será o expresso em documento fiscal ou contabil
 
-if (Debito != Credito):
+if (Debito != Credito): # essa condição foi para demonstrar o lançamento contábil. Daqui poderá extratir os livros razão e Diario.
     print( 'Debito {}' .format(Debito))
     print('Credito {}' .format(Credito))
     print('R$ {}' .format(Valor))
-else:
+else:  # aqui essa condição foi para que as contas debito e credito não sejam iguai, pois o balancete irá ter ATIVO diferente de Passivo.
     print('conta debito não pode ser igual a debito')
 
